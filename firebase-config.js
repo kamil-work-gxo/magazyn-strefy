0c675e00bdf3db435b834704f89dd94821005b9c
kamil-work-gxo <nowacki.kamil.praca@gmail.com>
2025-10-07 18:54:31 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getDatabase, ref, set, get, onValue, update } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "TWOJ_API_KEY",
+  apiKey: "AIzaSyAZzxQydgtPYdK7aVO9F7mei1I6e5f9Y7w",
   authDomain: "magazyn-strefy.firebaseapp.com",
-  databaseURL: "https://magazyn-strefy-default-rtdb.firebaseio.com",
+  databaseURL: "https://magazyn-strefy-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "magazyn-strefy",
-  storageBucket: "magazyn-strefy.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  storageBucket: "magazyn-strefy.firebasestorage.app",
+  messagingSenderId: "588000222819",
+  appId: "1:588000222819:web:9168e656dddb01c6fce256"
 };
 
 export const app = initializeApp(firebaseConfig);
